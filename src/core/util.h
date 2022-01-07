d261e21bc51385f152a9ff61c151e5909464ddb2
bjorn <mail@bjornbyt.es>
2022-01-07 08:46:43 +0200

    Add lovrUnreachable macro;
    Used instead of lovrThrow("Unreachable") so that it can be changed
    to do something more useful during debugging.

--- src/core/util.h
+++ src/core/util.h
@@ -36,6 +36,7 @@ typedef void errorFn(void*, const char*, va_list);
 void lovrSetErrorCallback(errorFn* callback, void* userdata);
 void LOVR_NORETURN lovrThrow(const char* format, ...);
 #define lovrAssert(c, ...) if (!(c)) { lovrThrow(__VA_ARGS__); }
+#define lovrUnreachable() lovrThrow("Unreachable")
 
 #ifdef LOVR_UNCHECKED
 #define lovrCheck(c, ...) ((void) 0)
